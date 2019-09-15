0a44e8925e0d4bd66ef27901190b4fcf8d8c747f
unknown <pjansen@lakeheadu.ca>
2019-09-15 16:01:40 -0400

    Completed Lab 2 Main Tasks

---
+++ Lab2/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(string category)
+        {
+            ViewBag.category = category;
+            return View();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+<h1>COMP2084 - Lab 2</h1>
+<p>Project created for COMP2084 Fall 2019 course for Lakehead-Georgian University by Peter Jansen.</p>
+<p>All Contents and assets have been used for educational Purposes</p>
+<p>last updated - September 15, 2019</p>
